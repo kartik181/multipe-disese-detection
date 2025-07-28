043717cb3a4763fccc5f408ea317f792eb785fcd
kartik181 <kartiksethi6798@gmail.com>
2025-07-28 10:14:03 +0530

    fixes

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-
 # Set page configuration
 st.set_page_config(page_title="Health Assistant",
                    layout="wide",
